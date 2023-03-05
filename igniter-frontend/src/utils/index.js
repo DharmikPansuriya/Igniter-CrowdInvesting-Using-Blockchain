44faad5929557cfd22da158a2efa209cef7a41be
DharmikEthan <intern2@ethan-ai.com>
2023-03-05 14:52:16 +0530

    Comments added into Utils

--- igniter-frontend/src/utils/index.js
+++ igniter-frontend/src/utils/index.js
@@ -1,3 +1,4 @@
+// Calculate number of remaining days left
 export const daysLeft = (deadline) => {
   const difference = new Date(deadline).getTime() - Date.now();
   const remainingDays = difference / (1000 * 3600 * 24);
@@ -5,12 +6,14 @@ export const daysLeft = (deadline) => {
   return remainingDays.toFixed(0);
 };
 
+// Calculate how much percent project is funded
 export const calculateBarPercentage = (goal, raisedAmount) => {
   const percentage = Math.round((raisedAmount * 100) / goal);
 
   return percentage;
 };
 
+// Check if image url is valid or not
 export const checkIfImage = (url, callback) => {
   const img = new Image();
   img.src = url;
