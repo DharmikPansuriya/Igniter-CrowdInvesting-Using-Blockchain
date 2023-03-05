873a5a88075fad377e1777ab0819eb807227ac63
DharmikEthan <intern2@ethan-ai.com>
2023-03-05 12:53:42 +0530

    Smart Contract Comment Added

--- igniter-web3-backend/contracts/CrowdInvest.sol
+++ igniter-web3-backend/contracts/CrowdInvest.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 contract CrowdInvest {
+    // Structure of block is created, this is very important. As this order will be followed while pushing the data into blockchain.
     struct Project {
         address founder;
         string title;
@@ -13,10 +14,15 @@ contract CrowdInvest {
         address[] investors;
         uint256[] investment;
     }
+
+    // Project Struct is mapped to projects, so it can be accessible in this smart contract
     mapping(uint256 => Project) public projects;
 
+    // This variable will maintain total count of project
     uint256 public projectCount = 0;
 
+    // This function will create a block and push the data into blockchain.
+    // This function can write into blockchain.
     function createProject(address _founder,
         string memory _title,
         string memory _description,
@@ -40,6 +46,8 @@ contract CrowdInvest {
         return projectCount;
     }
 
+    // This function will be triggered when investor wants to make investment in any startup.
+    // This function can write into blockchain.
     function investToProject(uint256 _id) public payable {
         uint256 amount = msg.value;
         Project storage project = projects[_id];
@@ -54,10 +62,13 @@ contract CrowdInvest {
         }
     }
 
+    // This function will return list of investor of one partcular project using id.
+    // This function is read only
     function getInvestors(uint256 _id) view public returns (address[] memory, uint256[] memory) {
         return (projects[_id].investors, projects[_id].investment);
     }
 
+    // This function will return all the project recored into our blockchain.
     function getProjects() public view returns (Project[] memory){
         Project[] memory result = new Project[](projectCount);
 
